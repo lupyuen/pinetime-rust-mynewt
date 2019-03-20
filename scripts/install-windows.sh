e4aea4bf9508b5daf514116812fe86ea13d5a869
Lee Lup Yuen <luppy@appkaki.com>
2019-03-21 03:22:36 +0800

    Install Windows

--- scripts/install-windows.sh
+++ scripts/install-windows.sh
@@ -13,8 +13,19 @@ set -x  #  Echo all commands.
 ##sudo apt install nodejs -y
 ##node --version
 
-#  Install Arm Toolchain. From https://gnu-mcu-eclipse.github.io/toolchain/arm/install/
-sudo npm install --global xpm
-sudo xpm install --global @gnu-mcu-eclipse/arm-none-eabi-gcc
+#  Install Arm Toolchain into $HOME/opt/xPacks/@gnu-mcu-eclipse/arm-none-eabi-gcc/*/.content/. From https://gnu-mcu-eclipse.github.io/toolchain/arm/install/
+##sudo npm install --global xpm
+##sudo xpm install --global @gnu-mcu-eclipse/arm-none-eabi-gcc
+
+#  Add Arm Toolchain to front of PATH.
+##echo export PATH=`ls -d $HOME/opt/xPacks/@gnu-mcu-eclipse/arm-none-eabi-gcc/*/.content/bin`:\$PATH >> ~/.bashrc
+##export PATH=`ls -d $HOME/opt/xPacks/@gnu-mcu-eclipse/arm-none-eabi-gcc/*/.content/bin`:$PATH
+arm-none-eabi-gcc --version  #  Should show "gcc version 8.2.1 20181213" or later.
+
+#  Upgrade git to prevent "newt install" error: "Unknown subcommand: get-url".
+sudo add-apt-repository ppa:git-core/ppa -y
+sudo apt-get update
+sudo apt-get install git -y
+git --version  #  Should show "git version 2.21.0" or later.
 
 echo "Done!"
