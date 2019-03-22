31327234c14384b9257303573e3aaa2b60b35509
lupyuen <luppy@appkaki.com>
2019-03-22 23:17:27 +0800

    Added install extensions

---
+++ scripts/install-windows-ext.cmd
@@ -0,0 +1,9 @@
+::  Install Visual Studio Code Extensions for Windows.
+
+call code --install-extension sanaajani.taskrunnercode --force
+
+call code --install-extension marus25.cortex-debug --force
+
+call code --install-extension ms-vscode.cpptools --force
+
+echo "Done!"
