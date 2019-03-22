ab3a25d95666fab165ae2cbc7d9ea9082e3f13cf
lupyuen <luppy@appkaki.com>
2019-03-22 23:20:07 +0800

    Renamed install ext

--- scripts/install-windows-ext.cmd
+++
@@ -1,9 +0,0 @@
-::  Install Visual Studio Code Extensions for Windows.
-
-call code --install-extension sanaajani.taskrunnercode --force
-
-call code --install-extension marus25.cortex-debug --force
-
-call code --install-extension ms-vscode.cpptools --force
-
-echo "Done!"
