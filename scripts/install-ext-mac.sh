aacb580262031ad9cfcc438ee93b4557564a8cd2
lupyuen <luppy@appkaki.com>
2019-03-26 01:43:30 +0800

    Install mac extensions

---
+++ scripts/install-ext-mac.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#  Install Visual Studio Code Extensions for macOS.  Based on https://code.visualstudio.com/docs/setup/mac
+
+set -e  #  Exit when any command fails.
+set -x  #  Echo all commands.
+
+#  Where the VSCode executable is located.
+codedir="/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
+
+#  Install the Task Running Extension which allows us to run tasks by clicking the task in the Task Runner pane.
+"$codedir/code" --install-extension sanaajani.taskrunnercode --force
+
+#  Install the Cortex Debugger Extension for debugging Blue Pill programs.
+"$codedir/code" --install-extension marus25.cortex-debug --force
+
+#  Install C++ Intellisense to provide C++ code navigation.
+"$codedir/code" --install-extension ms-vscode.cpptools --force
+
+set +x  #  Stop echoing all commands.
+echo "**** Done! Please restart Visual Studio Code to activate the extensions"
