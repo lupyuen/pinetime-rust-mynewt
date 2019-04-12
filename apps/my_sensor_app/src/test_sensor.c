5ee12173baf05a57c84fff00e819d933e3c7548d
lupyuen <luppy@appkaki.com>
2019-04-13 02:09:01 +0800

    rawtemp: 1726 / temp: 277.3

--- apps/my_sensor_app/src/test_sensor.c
+++ apps/my_sensor_app/src/test_sensor.c
@@ -143,8 +143,8 @@ void MX_ADC1_Init(void) {
   hadc1.Init.ScanConvMode = DISABLE;  //  Disable scan conversion mode
 
   // adc_set_single_conversion_mode(ADC1);
-  // hadc1.Init.ContinuousConvMode = DISABLE;  //  Disable continuous conversion mode
-  hadc1.Init.ContinuousConvMode = ENABLE;  //  Enable continuous conversion mode
+  hadc1.Init.ContinuousConvMode = DISABLE;  //  Disable continuous conversion mode
+  // hadc1.Init.ContinuousConvMode = ENABLE;  //  Enable continuous conversion mode
   //// Previously: ENABLE;
 
   // ???
