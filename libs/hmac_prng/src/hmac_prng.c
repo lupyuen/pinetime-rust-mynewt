3df5e83abafe785d5142c9b5754ce5860cdfba3f
lupyuen <luppy@appkaki.com>
2019-04-13 20:50:56 +0800

    Added doc

--- libs/hmac_prng/src/hmac_prng.c
+++ libs/hmac_prng/src/hmac_prng.c
@@ -19,35 +19,34 @@ int hmac_prng_init(void) {
     int rc;
 #define ENTROPY
 #ifdef ENTROPY
-    //  stm32f1_adc_create();  temp_stm32_create();
-    struct temp_stm32 *dev = (struct temp_stm32 *) os_dev_open(TEMP_STM32_DEVICE, OS_TIMEOUT_NEVER, NULL);
-    assert(dev);
+    //  Use the internal temperature sensor as a source of entropy (noise) for the generator.
     int rawtemp;
-    rc = temp_stm32_get_raw_temperature(dev, sizeof(seed) * 2, &rawtemp, seed);
-    assert(rc == 0);
-    os_dev_close((struct os_dev *) dev);
+    //  Open the temp_stm32 internal temperature sensor driver.  Assumes already started with "stm32f1_adc_create();  temp_stm32_create();""
+    struct temp_stm32 *dev = (struct temp_stm32 *) os_dev_open(TEMP_STM32_DEVICE, OS_TIMEOUT_NEVER, NULL);  assert(dev);
+
+    //  Fetch 32 bytes of temperature noise into the seed array.
+    rc = temp_stm32_get_raw_temperature(dev, sizeof(seed) * 2, &rawtemp, seed); assert(rc == 0);
+    os_dev_close((struct os_dev *) dev);  //  Close the driver.
 #endif  //  ENTROPY
 
-    //  Personalise the random number generator with the hardware ID.
-    hw_id_len = hal_bsp_hw_id_len();
+    //  Fetch the hardware ID.  This is unique across all microcontrollers.  
+    hw_id_len = hal_bsp_hw_id_len();     //  Fetch the length, i.e. 12
     assert(hw_id_len >= sizeof(hw_id));  //  Hardware ID too short.
+    hw_id_len = hal_bsp_hw_id(hw_id, sizeof(hw_id));  assert(hw_id_len > 0);  //  Get the hardware ID.
 
-    hw_id_len = hal_bsp_hw_id(hw_id, sizeof(hw_id));
-    assert(hw_id_len > 0);
+    //  Personalise the random number generator with the hardware ID.
+    //  TinyCrypt uses the hardware ID to prevent generating similar random numbers across microcontrollers.
     console_printf("Personalising random number generator with\n");  ////
     console_printf("hw_id: "); console_dump(hw_id, hw_id_len); console_printf("\n"); console_flush(); ////
-
-    //  int tc_hmac_prng_init(TCHmacPrng_t prng, const uint8_t *personalization, unsigned int plen);
-    rc = tc_hmac_prng_init(&prng, hw_id, hw_id_len);
-    assert(rc);
+    rc = tc_hmac_prng_init(&prng, hw_id, hw_id_len);  assert(rc);
 
 #ifdef ENTROPY
-    console_printf("SEEDING WITH TEMPERATURE SENSOR ENTROPY:\n");
-    console_dump(seed, sizeof(seed)); console_printf("\n"); console_flush(); ////
+    console_printf("SEEDING WITH TEMPERATURE SENSOR ENTROPY:\n"); console_dump(seed, sizeof(seed)); console_printf("\n"); console_flush(); ////
 #else
     console_printf("NO ENTROPY\n");
 #endif  //  ENTROPY
-    //  Seed must be >= 32 bytes long.
+
+    //  Use the temperature entropy data to seed the generator.  Seed must be >= 32 bytes long.
     rc = tc_hmac_prng_reseed(&prng, seed, sizeof(seed), NULL, 0);
     assert(rc);
     return 0;
@@ -56,7 +55,7 @@ int hmac_prng_init(void) {
 int hmac_prng_generate(uint8_t *rnd, unsigned int rndlen) {
     //  Generate a random number of length rndlen bytes and save to rnd.  Return 0 if successful.
     int rc;
-    rc = tc_hmac_prng_generate(rnd, rndlen, &prng);
-    assert(rc);
+    //  Call TinyCrypt to generate the random number.
+    rc = tc_hmac_prng_generate(rnd, rndlen, &prng);  assert(rc);
     return 0;
 }
