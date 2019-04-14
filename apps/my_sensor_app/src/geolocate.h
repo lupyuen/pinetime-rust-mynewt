95029239ba109a02dee0649d9ec2d40e807e6fcd
lupyuen <luppy@appkaki.com>
2019-04-15 06:45:36 +0800

    Added doc

--- apps/my_sensor_app/src/geolocate.h
+++ apps/my_sensor_app/src/geolocate.h
@@ -14,6 +14,7 @@ struct oc_server_handle;
 //  Scan for WiFi access points in your area.  Send the MAC Address and signal strength of
 //  the first 3 access points (or fewer) to thethings.io at the specified CoAP server and uri.  
 //  dev is the ESP8266 device.  Return the number of access points transmitted.
+//  The caller must have locked the ESP8266 driver via os_dev_open() to prevent concurrent access.
 //  Note: Don't enable this unless you understand the privacy implications. Your location may be accessible by others.
 int geolocate(struct esp8266 *dev, struct oc_server_handle *server, const char *uri);
 
