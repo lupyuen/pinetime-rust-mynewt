ad97ceabd50b94369c3b4c11dd6846a01a23a6b6
lupyuen <luppy@appkaki.com>
2019-05-01 04:15:27 +0800

    Adding raw temp to temp_stm32

--- libs/remote_sensor/src/remote_sensor.c
+++ libs/remote_sensor/src/remote_sensor.c
@@ -38,16 +38,6 @@ static const struct sensor_driver g_sensor_driver = {
 };
 
 /////////////////////////////////////////////////////////
-//  Custom Sensor Data Definitions
-
-//  Raw Temperature Sensor
-#define SENSOR_TYPE_AMBIENT_TEMPERATURE_RAW SENSOR_TYPE_USER_DEFINED_1
-struct sensor_temp_raw_data {   //  Represents a single temperature sensor raw value
-    uint32_t strd_temp_raw;     //  Raw temp from STM32 Internal Temp Sensor is 0 to 4095.
-    uint8_t  strd_temp_raw_is_valid:1;  //  1 if data is valid
-} __attribute__((packed));
-
-/////////////////////////////////////////////////////////
 //  Sensor Data Union
 
 typedef union {  //  Union that represents all possible sensor values
