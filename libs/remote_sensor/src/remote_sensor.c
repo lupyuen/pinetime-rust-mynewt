e71ab697519c6b2cec30f5de38edc5de592ee260
lupyuen <luppy@appkaki.com>
2019-04-30 08:09:49 +0800

    Adding raw temp

--- libs/remote_sensor/src/remote_sensor.c
+++ libs/remote_sensor/src/remote_sensor.c
@@ -26,6 +26,8 @@
 //  Exports for the sensor API
 static int sensor_read(struct sensor *, sensor_type_t, sensor_data_func_t, void *, uint32_t);
 static int sensor_get_config(struct sensor *, sensor_type_t, struct sensor_cfg *);
+static int sensor_open(struct os_dev *dev0, uint32_t timeout, void *arg);
+static int sensor_close(struct os_dev *dev0);
 
 //  Global instance of the sensor driver
 static const struct sensor_driver g_sensor_driver = {
@@ -33,33 +35,8 @@ static const struct sensor_driver g_sensor_driver = {
     sensor_get_config
 };
 
-int remote_sensor_default_cfg(struct remote_sensor_cfg *cfg) {
-    //  Return the default sensor configuration.
-    memset(cfg, 0, sizeof(struct remote_sensor_cfg));  //  Zero the entire object.
-    cfg->bc_s_mask = SENSOR_TYPE_ALL;  //  Return all sensor values, i.e. temperature.
-    return 0;
-}
-
-static int sensor_open(struct os_dev *dev0, uint32_t timeout, void *arg) {
-    //  Setup the sensor.  Return 0 if successful.
-    int rc = -1;
-    struct remote_sensor *dev;    
-    struct remote_sensor_cfg *cfg;
-    dev = (struct remote_sensor *) dev0;  assert(dev);  
-    cfg = &dev->cfg; assert(cfg);
-
-    return 0;
-err:
-    assert(rc == 0);
-    return rc;
-}
-
-static int sensor_close(struct os_dev *dev0) {
-    //  Close the sensor.  Return 0 if successful.
-    struct remote_sensor *dev;    
-    dev = (struct remote_sensor *) dev0;
-    return 0;
-}
+/////////////////////////////////////////////////////////
+//  Device Creation Functions
 
 /**
  * Expects to be called back through os_dev_create().
@@ -105,44 +82,6 @@ err:
     return (rc);
 }
 
-static int sensor_read(struct sensor *sensor, sensor_type_t type,
-    sensor_data_func_t data_func, void *data_arg, uint32_t timeout) {
-    //  Read the sensor values depending on the sensor types specified in the sensor config.
-    union {  //  Union that represents all possible sensor values.
-        struct sensor_temp_data std;  //  Temperature sensor value.
-    } databuf;
-    struct remote_sensor *dev;
-    int rc, rawtemp;
-    float temp;
-
-    //  We only allow reading of temperature values.
-    if (!(type & SENSOR_TYPE_AMBIENT_TEMPERATURE)) { rc = SYS_EINVAL; goto err; }
-    dev = (struct remote_sensor *) SENSOR_GET_DEVICE(sensor); assert(dev);
-    rawtemp = -1;
-    {   //  Begin ADC Lock: Open and lock port ADC1, configure channel 16.
-        rc = sensor_open((struct os_dev *) dev, 0, NULL);
-        if (rc) { goto err; }
-
-        //  Get a new temperature sample from temperature sensor (channel 16 of port ADC1).
-        rc = remote_sensor_get_raw_temperature(dev, 1, &rawtemp, NULL);
-
-        sensor_close((struct os_dev *) dev);
-    }   //  End ADC Lock: Close and unlock port ADC1.
-    if (rc) { goto err; }  //  console_printf("rawtemp: %d\n", rawtemp);  ////
-
-    //  Save the temperature.
-    databuf.std.std_temp = temp;
-    databuf.std.std_temp_is_valid = 1;  //  console_printf("temp: ");  console_printfloat(temp);  console_printf("\n");  ////
-    
-    if (data_func) {  //  Call the listener function to process the sensor data.
-        rc = data_func(sensor, data_arg, &databuf.std, SENSOR_TYPE_AMBIENT_TEMPERATURE);
-        if (rc) { goto err; }
-    }
-    return 0;
-err:
-    return rc;
-}
-
 static int sensor_get_config(struct sensor *sensor, sensor_type_t type,
     struct sensor_cfg *cfg) {
     //  Return the type of the sensor value returned by the sensor.
@@ -177,3 +116,79 @@ int remote_sensor_config(struct remote_sensor *dev, struct remote_sensor_cfg *cf
 err:
     return (rc);
 }
+
+int remote_sensor_default_cfg(struct remote_sensor_cfg *cfg) {
+    //  Return the default sensor configuration.
+    memset(cfg, 0, sizeof(struct remote_sensor_cfg));  //  Zero the entire object.
+    cfg->bc_s_mask = SENSOR_TYPE_ALL;  //  Return all sensor values, i.e. temperature.
+    return 0;
+}
+
+/////////////////////////////////////////////////////////
+//  Device Open and Close Functions
+
+static int sensor_open(struct os_dev *dev0, uint32_t timeout, void *arg) {
+    //  Setup the sensor.  Return 0 if successful.
+    int rc = -1;
+    struct remote_sensor *dev;    
+    struct remote_sensor_cfg *cfg;
+    dev = (struct remote_sensor *) dev0;  assert(dev);  
+    cfg = &dev->cfg; assert(cfg);
+
+    return 0;
+err:
+    assert(rc == 0);
+    return rc;
+}
+
+static int sensor_close(struct os_dev *dev0) {
+    //  Close the sensor.  Return 0 if successful.
+    struct remote_sensor *dev;    
+    dev = (struct remote_sensor *) dev0;
+    return 0;
+}
+
+/////////////////////////////////////////////////////////
+//  Read Sensor Functions
+
+static int sensor_read(struct sensor *sensor, sensor_type_t type,
+    sensor_data_func_t data_func, void *data_arg, uint32_t timeout) {
+    //  Read the sensor values depending on the sensor types specified in the sensor config.
+    union {  //  Union that represents all possible sensor values.
+        struct sensor_temp_data std;       //  Temperature sensor value
+        struct sensor_temp_raw_data strd;  //  Temperature sensor raw value
+        struct sensor_press_data spd;      //  Pressure sensor value
+        struct sensor_humid_data shd;      //  Humidity sensor value
+    } databuf;
+    struct remote_sensor *dev;
+    int rc, rawtemp;
+    float temp;
+
+    //  We only allow reading of temperature values.
+    if (!(type & SENSOR_TYPE_AMBIENT_TEMPERATURE)) { rc = SYS_EINVAL; goto err; }
+    dev = (struct remote_sensor *) SENSOR_GET_DEVICE(sensor); assert(dev);
+    rawtemp = -1;
+    {   //  Begin ADC Lock: Open and lock port ADC1, configure channel 16.
+        rc = sensor_open((struct os_dev *) dev, 0, NULL);
+        if (rc) { goto err; }
+
+        //  Get a new temperature sample from temperature sensor (channel 16 of port ADC1).
+        rc = remote_sensor_get_raw_temperature(dev, 1, &rawtemp, NULL);
+
+        sensor_close((struct os_dev *) dev);
+    }   //  End ADC Lock: Close and unlock port ADC1.
+    if (rc) { goto err; }  //  console_printf("rawtemp: %d\n", rawtemp);  ////
+
+    //  Save the temperature.
+    databuf.std.std_temp = temp;
+    databuf.std.std_temp_is_valid = 1;  //  console_printf("temp: ");  console_printfloat(temp);  console_printf("\n");  ////
+    
+    if (data_func) {  //  Call the listener function to process the sensor data.
+        rc = data_func(sensor, data_arg, &databuf.std, SENSOR_TYPE_AMBIENT_TEMPERATURE);
+        if (rc) { goto err; }
+    }
+    return 0;
+err:
+    return rc;
+}
+
