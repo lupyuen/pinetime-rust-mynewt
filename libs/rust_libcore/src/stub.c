62bd30c52093259a8dd50d88a4b6891afb62621c
lupyuen <luppy@appkaki.com>
2019-06-04 21:08:09 +0800

    Adding Rust stubs

---
+++ libs/rust_libcore/src/stub.c
@@ -0,0 +1,4 @@
+#include <sysinit/sysinit.h>  //  Contains all app settings consolidated from "apps/my_sensor_app/syscfg.yml" and "targets/bluepill_my_sensor/syscfg.yml"
+
+void rust_libcore_stub() {
+}
