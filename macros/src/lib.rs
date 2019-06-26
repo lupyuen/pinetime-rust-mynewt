09ce68df2bb14b67332aa87d1536709608bb048d
lupyuen <luppy@appkaki.com>
2019-06-26 20:32:45 +0800

    Added proc macros

---
+++ macros/src/lib.rs
@@ -0,0 +1,45 @@
+extern crate proc_macro;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input};
+
+#[proc_macro]
+pub fn run(item: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(item as syn::Block);
+    for stmt in input.stmts {
+        //  println!("stmt: {:#?}", quote! { #stmt });
+        //  println!("id: {:#?}", stmt.ident);
+        match stmt {
+            syn::Stmt::Local(local) => {
+            }
+            syn::Stmt::Item(item) => {
+            }
+            syn::Stmt::Expr(expr) => {
+            }
+            syn::Stmt::Semi(expr, _semi) => {
+                //  println!("expr: {:#?}", quote! { #expr });
+                match expr {
+                    syn::Expr::Call(expr2) => {
+                        println!("expr2: {:#?}", quote! { #expr2 });
+                    }
+                    _ => {
+                        assert!(false);
+                    }
+                }
+            
+            }
+        }
+    }
+    "fn answer() -> u32 { 42 }".parse().unwrap()    
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test1() {
+        assert_eq!(1, 1);
+    }
+}
