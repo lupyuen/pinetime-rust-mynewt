25d21dfad393f631f573eaac5d5143377f1af8c6
lupyuen <luppy@appkaki.com>
2019-06-27 16:37:26 +0800

    Added doc

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -10,7 +10,28 @@ pub fn safe_wrap(attr: TokenStream, item: TokenStream) -> TokenStream {
     "// Hello world".parse().unwrap()
 }
 
-/// Run a block of CBOR encoding calls with error checking.
+/// Transform a block of CBOR encoding calls by adding error checking. Given input:
+/// ```
+/// let encoder = JSON_CONTEXT.encoder("JSON_CONTEXT", "_map");
+/// cbor_encode_text_string(
+///     encoder,
+///     JSON_CONTEXT.key_to_cstr(key_with_opt_null),
+///     JSON_CONTEXT.cstr_len(key_with_opt_null));
+/// cbor_encode_int(encoder, value);
+/// ```
+/// Return:
+/// ```
+/// unsafe {
+///     let encoder = JSON_CONTEXT.encoder("JSON_CONTEXT", "_map");
+///     let res =
+///         tinycbor::cbor_encode_text_string(encoder,
+///           JSON_CONTEXT.key_to_cstr(key_with_opt_null),
+///           JSON_CONTEXT.cstr_len(key_with_opt_null));
+///     JSON_CONTEXT.check_result(res);
+///     let res = tinycbor::cbor_encode_int(encoder, value);
+///     JSON_CONTEXT.check_result(res);
+/// };
+/// ```
 #[proc_macro]
 pub fn run(item: TokenStream) -> TokenStream {
     //  Parse the macro input as a block of statements.
@@ -26,10 +47,10 @@ pub fn run(item: TokenStream) -> TokenStream {
             //  If this is a statement followed by a semicolon...
             syn::Stmt::Semi(expr, _semi) => {
                 match expr {
-                    //  If statement is a function call like `func(...)`
+                    //  If statement is a function call like `func(...)`...
                     syn::Expr::Call(expr) => {                        
-                        let func = *expr.func;  //  Get the function called.
-                        let func = quote! { #func };  //  Convert to token form.
+                        let func = *expr.func;        //  Get the function called.
+                        let func = quote! { #func };  //  Summarise as token form.
                         //  If this is a CBOR encoding call..
                         if func.to_string().starts_with("cbor_encode_") {
                             //  Add error checking to the CBOR statement.
@@ -38,7 +59,7 @@ pub fn run(item: TokenStream) -> TokenStream {
                                 JSON_CONTEXT.check_result(res);
                             };
                             //  Append updated statement tokens to result.
-                            expanded = quote! { #expanded #updated_stmt };  
+                            expanded.extend(updated_stmt);  
                             continue;  //  Skip to next statement.
                         }
                     }
@@ -48,13 +69,13 @@ pub fn run(item: TokenStream) -> TokenStream {
             }
             //  If this is a statement not followed by a semicolon...
             syn::Stmt::Expr(_expr) => {}  //  TOOD
-            //  If this is a `let` statement like `let a = ...`
+            //  If this is a `let` statement like `let a = ...`...
             syn::Stmt::Local(_local) => {}  //  TODO
-            //  If this is an item definition like `const a = ...`
+            //  If this is an item definition like `const a = ...`...
             syn::Stmt::Item(_item) => {}  //  TODO
         }
         //  If we reach here, this statement is not a CBOR encoding call.  Return verbatim.
-        expanded = quote! { #expanded #stmt_tokens };  //  Append statement tokens to result.
+        expanded.extend(stmt_tokens);  //  Append statement tokens to result.
     }
     //  Wrap the expanded tokens with an `unsafe` block.
     expanded = quote! {
