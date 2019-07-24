6af1a14adc0db117b8c565753711666fb511b632
lupyuen <luppy@appkaki.com>
2019-07-24 10:35:54 +0800

    Updated Windows scripts

--- scripts/install-ext-windows.cmd
+++ scripts/install-ext-windows.cmd
@@ -1,18 +1,19 @@
-::  Install Visual Studio Code Extensions for Windows.
+::  Install Visual Studio Code Extensions for Windows. %1 is the path of code.exe
+echo %1
 
 ::  Install the Task Running Extension which allows us to run tasks by clicking the task in the Task Runner pane.
-call code --install-extension sanaajani.taskrunnercode --force
+%1 --install-extension sanaajani.taskrunnercode --force
 
 ::  Install the Cortex Debugger Extension for debugging Blue Pill programs.
-call code --install-extension marus25.cortex-debug --force
+%1 --install-extension marus25.cortex-debug --force
 
 ::  Install C++ Intellisense to provide C++ code navigation.
-call code --install-extension ms-vscode.cpptools --force
+%1 --install-extension ms-vscode.cpptools --force
 
 ::  Install Rust Language Service Extension to provide Rust code navigation.
-call code --install-extension rust-lang.rust --force
+%1 --install-extension rust-lang.rust --force
 
 ::  Install Better TOML Extension for editing Config.toml in Rust.
-call code --install-extension bungcip.better-toml --force
+%1 --install-extension bungcip.better-toml --force
 
 echo ✅ ◾ ️Done! Please restart Visual Studio Code to activate the extensions
