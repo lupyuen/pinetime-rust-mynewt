8b8bb33b823b5b15a3d767fd9fe5fa7ebc6197e9
lupyuen <luppy@appkaki.com>
2019-07-26 03:04:08 +0800

    Adding STM32L4 scripts

---
+++ scripts/stm32l4/README.md
@@ -0,0 +1,3 @@
+# stm32l4
+
+Scripts for STM32L4
---
+++ scripts/stm32l4/flash-app.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+#  Flash Mynewt Bootloader
+
+set -e  #  Exit when any command fails.
+set -x  #  Echo all commands.
+
+openocd/bin/openocd -f scripts/flash-init.ocd -f interface/stlink-v2.cfg -f target/stm32l4x.cfg -f scripts/stm32l4/flash-app.ocd
