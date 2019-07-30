993d8c58e359fc36f9cdf22ab08ed154ff16dfd2
lupyuen <luppy@appkaki.com>
2019-07-30 23:52:58 +0800

    Fixed namespace

--- rust/macros/src/safe_wrap.rs
+++ rust/macros/src/safe_wrap.rs
@@ -32,15 +32,9 @@ pub fn safe_wrap_internal(_attr: TokenStream, item: TokenStream) -> TokenStream 
         if let syn::ForeignItem::Fn(foreign_fn) = foreign_item {
             //  println!("foreign_fn: {:#?}", foreign_fn);
             //  Get the function name, with and without namespace (`os_task_init` vs `task_init`)
-            let fn_name = foreign_fn.ident.to_string();
-            let namespace = get_namespace(&fn_name);
-            //let namespace = "os";  //  TODO
-            let namespace_prefix = format!("{}_", namespace).to_string();  //  e.g. `os_`
-
-            assert!(fn_name.starts_with(&namespace_prefix));
-            let fn_name_without_namespace = &fn_name[namespace_prefix.len()..];
-            let fn_name_token = syn::Ident::new(&fn_name, foreign_fn.ident.span());
-            let fn_name_without_namespace_token = syn::Ident::new(&fn_name_without_namespace, foreign_fn.ident.span());
+            let transformed_fname = transform_function_name(&foreign_fn.ident);
+            let fname_token = transformed_fname.token;
+            let fname_without_namespace_token = transformed_fname.without_namespace_token;
 
             let return_type_str =
                 if let ReturnType::Type (_, return_type) = foreign_fn.decl.output {
@@ -48,7 +42,7 @@ pub fn safe_wrap_internal(_attr: TokenStream, item: TokenStream) -> TokenStream 
                     (quote! { #return_type }).to_string()
                 }
                 else { "".to_string() };  //  No return type
-            println!("return_type_str: {:#?}", return_type_str);
+            //  println!("return_type_str: {:#?}", return_type_str);
 
             //  Get the function args and transform each arg into 3 forms:
             //  (1) Wrap Declaration: How the arg type is exposed via the wrapper
@@ -96,18 +90,18 @@ pub fn safe_wrap_internal(_attr: TokenStream, item: TokenStream) -> TokenStream 
             //  Compose the wrapper code as tokens.
             let expanded = quote! {
                 //  "----------Insert Func Name: `pub fn task_init() -> {`----------";
-                pub fn #fn_name_without_namespace_token(
+                pub fn #fname_without_namespace_token(
                     //  "----------Insert Wrapped Decl----------";
                     #wrap_tokens
                     /* Like this:
-                        t: Out<os_task>,  //  TODO: *mut os_task
-                        name: &Strn,      //  TODO: *const ::cty::c_char
+                        t: Out<os_task>,  //  Previously: *mut os_task
+                        name: &Strn,      //  Previously: *const ::cty::c_char
                         func: os_task_func_t,
-                        arg: Ptr,         //  TODO: *mut ::cty::c_void
+                        arg: Ptr,         //  Previously: *mut ::cty::c_void
                         prio: u8,
                         sanity_itvl: os_time_t,
-                        stack_bottom: Out<[os_stack_t]>,  //  TODO: *mut os_stack_t
-                        stack_size: usize,                //  TODO: u16 */
+                        stack_bottom: Out<[os_stack_t]>,  //  Previously: *mut os_stack_t
+                        stack_size: usize,                //  Previously: u16 */
                 ) -> #declare_result_tokens {             //  e.g. MynewtResult<()> or MynewtResult<* mut os_eventq>
                     "----------Insert Extern Decl: `extern C { pub fn ... }`----------";
                     extern "C" { #foreign_item_tokens }
@@ -115,17 +109,17 @@ pub fn safe_wrap_internal(_attr: TokenStream, item: TokenStream) -> TokenStream 
                     #validation_tokens
                     unsafe {
                     "----------Insert Call: `let result_code = os_task_init(`----------";
-                        #get_result_tokens #fn_name_token(
+                        #get_result_tokens #fname_token(
                             //  "----------Insert Call Expr----------";
                             #call_tokens
                             /* Like this:
                                 t,
-                                name.bytestr.as_ptr() as *const ::cty::c_char,  //  TODO
+                                name.bytestr.as_ptr() as *const ::cty::c_char,  //  Converted to pointer
                                 func,
                                 arg,
                                 prio,
                                 sanity_itvl,
-                                stack_bottom.as_ptr() as *mut os_stack_t,  //  TODO
+                                stack_bottom.as_ptr() as *mut os_stack_t,  //  Converted to pointer
                                 stack_size as u16  */
                         );
                         #return_result_tokens
@@ -139,12 +133,6 @@ pub fn safe_wrap_internal(_attr: TokenStream, item: TokenStream) -> TokenStream 
         break;
     }
     "// Should not come here".parse().unwrap()
-}
-
-/// Given a function name like `os_task_init`, return the namespace (`os`)
-fn get_namespace(fname: &str) -> &str {
-    let fname_split: Vec<&str> = fname.splitn(2, "_").collect();
-    fname_split[0]
 }
 
 /// Collect the Wrapped Declarations for all args. Return a TokenStream of the declarations for the wrapper function:
@@ -292,6 +280,103 @@ fn transform_arg(arg: ArgCaptured) -> TransformedArg {
     }
 }
 
+/// Transform the extern function name e.g. `os_task_init`
+fn transform_function_name(ident: &syn::Ident) -> TransformedFunctionName {
+    // Get namespace e.g. `os`
+    let fname = ident.to_string();
+    let namespace = get_namespace(&fname);
+    println!("namespace: {:#?}", namespace);
+    // Get namespace prefix e.g. `os_`
+    let namespace_prefix = 
+        if namespace.len() > 0 { 
+            format!("{}_", namespace).to_string()  //  e.g. `os_`
+        } else {
+            "".to_string()
+        };
+    // Transform the function name based on namespace.
+    assert!(fname.starts_with(&namespace_prefix));
+    let fname_without_namespace = &fname[namespace_prefix.len()..];
+    let fname_token = syn::Ident::new(&fname, ident.span());
+    let fname_without_namespace_token = syn::Ident::new(&fname_without_namespace, ident.span());
+    //  Return the transformed function name.
+    TransformedFunctionName {
+        ident:                      Box::new(fname.clone()),
+        namespace:                  Box::new(namespace.to_string()),
+        without_namespace:          Box::new(fname_without_namespace.to_string()),
+        token:                      Box::new(fname_token),
+        without_namespace_token:    Box::new(fname_without_namespace_token),
+        ident_span:                 Box::new(ident.span()),
+    }
+}
+
+/// Given a function name like `os_task_init`, return the namespace (`os`)
+fn get_namespace(fname: &str) -> &str {
+    //  Get the first part before `_`.
+    let fname_split: Vec<&str> = fname.splitn(2, "_").collect();
+    let namespace = fname_split[0];
+    //  Match the namespace and ignore if it's not a known namespace.
+    match namespace {
+        "start" => { "" }   //  `start` is not a valid namespace e.g. `start_server_transport()
+        "init"  => { "" }   //  `init` is not a valid namespace e.g. `init_server_post()
+        "do"    => { "" }   //  `do` is not a valid namespace e.g. `do_server_post()
+        _ => { namespace }  //  Else it's a valid namspace
+    }
+}
+
+/*
+/// Transform the extern return type for Wrap declaration, Validation statement and Call expression.
+fn transform_arg(arg: ArgCaptured) -> TransformedArg {
+    //  `arg` contains `pat : ty` e.g. `t : * mut os_task`
+    //  println!("arg: {:#?}", arg);
+    let ArgCaptured{ pat, ty, .. } = arg;
+    //  println!("pat: {}, ty: {}", quote!{ #pat }, quote!{ #ty });
+    let pat_span = pat.span();  //  syn::Pat
+    let ty_span = ty.span();    //  syn::Type
+    let ident = quote!{ #pat }.to_string();
+    let extern_type = quote!{ #ty }.to_string();
+    let mut wrap_type = quote!{ #ty }.to_string();
+    let mut validation_stmt = "".to_string();
+    let mut call_expr = quote!{ #pat }.to_string();
+    //  Match the type and transform accordingly.
+    match extern_type.as_str() {
+        //  * const :: cty :: c_char => &Strn
+        "* const :: cty :: c_char" => {
+            wrap_type = "&Strn".to_string();
+            //  e.g. Strn::validate_bytestr(name.bytestr)
+            validation_stmt = format!("Strn::validate_bytestr({}.bytestr)", ident);
+            //  e.g. name.bytestr.as_ptr()
+            call_expr = format!("{}.bytestr.as_ptr()", ident);
+        }
+        //  * mut :: cty :: c_void => Ptr
+        "* mut :: cty :: c_void" => {
+            wrap_type = "Ptr".to_string();
+        }
+        //  * mut os_task => Out<os_task>
+        "* mut os_task" => {
+            //  TODO: Use regex to match any `os_*`: https://rust-lang-nursery.github.io/rust-cookbook/text/regex.html
+            wrap_type = "Out<os_task>".to_string();
+        }
+        //  * mut os_stack_t => Out<[os_stack_t]>
+        "* mut os_stack_t" => {
+            wrap_type = "Out<[os_stack_t]>".to_string();
+            //  e.g. stack_bottom.as_ptr()
+            call_expr = format!("{}.as_ptr()", ident);
+        }
+        _ => {}
+    }
+    //  Return the transformed arg.
+    TransformedArg {
+        ident:           Box::new(ident),
+        extern_type:     Box::new(extern_type),
+        wrap_type:       Box::new(wrap_type),
+        validation_stmt: Box::new(validation_stmt),
+        call_expr:       Box::new(call_expr),
+        ident_span:      Box::new(pat_span),
+        type_span:       Box::new(ty_span),
+    }
+}
+*/
+
 /// Extern arg declaration transformed into the Wrap, Validation and Call forms 
 struct TransformedArg {
     /// Identifier e.g. `name`
@@ -308,6 +393,22 @@ struct TransformedArg {
     ident_span: Box<Span>,
     /// Span of the type (file location)
     type_span: Box<Span>,
+}
+
+/// Extern function name transformed
+struct TransformedFunctionName {
+    /// Identifier e.g. `os_task_init`
+    ident: Box<String>,
+    /// Namespace e.g. `os`. Or empty if no namespace.
+    namespace: Box<String>,
+    /// Function name without namespace e.g. `task_init`
+    without_namespace: Box<String>,
+    /// Token of function name e.g. `os_task_init`
+    token: Box<syn::Ident>,
+    /// Token of function name without namespace e.g. `task_init`
+    without_namespace_token: Box<syn::Ident>,
+    /// Span of the identifier (file location)
+    ident_span: Box<Span>,
 }
 
 /// Separator for composing declarations and call expressions
