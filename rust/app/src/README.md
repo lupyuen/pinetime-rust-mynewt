55f32f827bc6d2a4b8a10168f701899c2cdcf731
lupyuen <luppy@appkaki.com>
2019-07-31 09:29:33 +0800

    Updating app

--- rust/app/src/README.md
+++ rust/app/src/README.md
@@ -1,20 +1,18 @@
-# src: Rust Application Hosted On Mynewt
+# src: Rust Sensor Application Hosted On Mynewt
 
-This Rust application polls the internal temperature sensor every 10 seconds and displays the results.
+This Rust application polls the internal temperature sensor every 10 seconds and transmits to the CoAP server at thethings.io.
 
-The application is compiled as a Rust library `libmylib.rlib`, which is injected into the Mynewt build.
+The application is compiled as a Rust library `libmyapp.rlib`, which is injected into the Mynewt build.
 
 The application was ported from the C version at [`/apps/my_sensor_app`](/apps/my_sensor_app)
 
 [`lib.rs`](lib.rs): Main library module. Contains `main()`, called by Mynewt at startup, and the panic handler. Imports the modules below via the `mod` directive.
 
-[`base.rs`](base.rs): Common declarations
+[`app_base.rs`](app_base.rs): Common declarations
 
-[`sensor.rs`](sensor.rs): Imports the [Mynewt Sensor Framework API](https://mynewt.apache.org/latest/os/modules/sensor_framework/sensor_framework.html). Contains custom sensor declarations.
+[`app_sensor.rs`](app_sensor.rs): Calls the [Mynewt Sensor Framework API](https://mynewt.apache.org/latest/os/modules/sensor_framework/sensor_framework.html) to poll the [Blue Pill internal temperature sensor](/libs/temp_stm32) every 10 seconds, and register a Listener Function that will be called after each poll.
 
-[`listen_sensor.rs`](listen_sensor.rs): Calls the [Mynewt Sensor Framework API](https://mynewt.apache.org/latest/os/modules/sensor_framework/sensor_framework.html) to poll the [Blue Pill internal temperature sensor](/libs/temp_stm32) every 10 seconds, and register a Listener Function that will be called after each poll.
-
-[`send_coap.rs`](send_coap.rs): Called by the Listener Function after each poll of the internal temperature sensor.
+[`app_network.rs`](app_network.rs): Transmit sensor data. Called by the Listener Function after each poll of the internal temperature sensor.
 
 [View Rust Documentation](https://lupyuen.github.io/stm32bluepill-mynewt-sensor/rust/mylib/)
 
--- rust/app/src/lib.rs
+++ rust/app/src/lib.rs
@@ -1,4 +1,22 @@
-//!  Sensor app that reads sensor data from a temperature sensor and sends the sensor data to a CoAP server or Collector Node.
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+//!  Sensor app that reads sensor data from a temperature sensor and sends the sensor data to a CoAP server over NB-IoT.
 //!  Note that we are using a patched version of apps/my_sensor_app/src/vsscanf.c that
 //!  fixes ESP8266 response parsing bugs.  The patched file must be present in that location.
 //!  This is the Rust version of `https://github.com/lupyuen/stm32bluepill-mynewt-sensor/blob/rust/apps/my_sensor_app/OLDsrc/main.c`
@@ -10,47 +28,47 @@
 #![feature(proc_macro_hygiene)]  //  Allow Procedural Macros like `run!()`
 #![feature(custom_attribute)]    //  Allow Custom Attributes like `#[safe_wrap]`
 
-extern crate cortex_m;       //  Declare the external library `cortex_m`
-extern crate macros;         //  Declare the Mynewt Macros library
-extern crate mynewt;         //  Declare the Mynewt library
+extern crate cortex_m;                  //  Declare the external library `cortex_m`
+extern crate mynewt;                    //  Declare the Mynewt library
+extern crate macros as proc_macros;     //  Declare the Mynewt Procedural Macros library
 
 #[allow(dead_code)] //  Suppress warnings of unused constants and vars
-mod base;           //  Declare `base.rs` as Rust module `base`
+mod app_base;           //  Declare `app_base.rs` as Rust module `app_base`
 
-mod listen_sensor;  //  Declare `listen_sensor.rs` as Rust module `listen_sensor`
-mod send_coap;      //  Declare `send_coap.rs` as Rust module `send_coap`
+mod app_sensor;   //  Declare `app_sensor.rs` as Rust module `app_sensor`
+mod app_network;  //  Declare `app_network.rs` as Rust module `app_network`
 
-use core::panic::PanicInfo;     //  Import `PanicInfo` type which is used by `panic()` below
-use cortex_m::asm::bkpt;        //  Import cortex_m assembly function to inject breakpoint
-use mynewt::kernel::os;         //  Import Mynewt OS API
-use crate::base::*;             //  Import `base.rs` for common declarations
+use core::panic::PanicInfo;         //  Import `PanicInfo` type which is used by `panic()` below
+use cortex_m::asm::bkpt;            //  Import cortex_m assembly function to inject breakpoint
+use mynewt::kernel::os;             //  Import Mynewt OS API
+use mynewt::libs::sensor_network;   //  Import Mynewt Sensor Network Library
+use crate::app_base::*;             //  Import `app_base.rs` for common declarations
 
+///  Main program that initialises the sensor, network driver and starts reading and sending sensor data in the background.
 ///  main() will be called at Mynewt startup. It replaces the C version of the main() function.
 #[no_mangle]                     //  Don't mangle the name "main"
 extern "C" fn rust_main() -> ! {  //  Declare extern "C" because it will be called by Mynewt
-    //  Init Mynewt system.
-    unsafe { base::rust_sysinit()  };
-    unsafe { console_flush() };
+    //  Initialise the Mynewt packages and BME280 / temp_stm32 temperature sensor driver.
+    //  Start the CoAP / OIC Background Task to transmit CoAP messages.  Any startup
+    //  functions defined in pkg.yml of our custom drivers and libraries will be called by 
+    //  sysinit().  Here are the startup functions consolidated by Mynewt:
+    //  bin/targets/bluepill_my_sensor/generated/src/bluepill_my_sensor-sysinit-app.c
+    unsafe { app_base::rust_sysinit(); console_flush() };
 
-    //  Start the Network Task in the background.  The Network Task prepares the ESP8266 or nRF24L01 transceiver for
-    //  sending CoAP messages.  We connect the ESP8266 to the WiFi access point and register
-    //  the ESP8266/nRF24L01 driver as the network transport for CoAP.  Also perform WiFi Geolocation if it is enabled.
-    send_coap::start_network_task()
-        .expect("NET fail");
-
-    //  Starting polling the temperature sensor every 10 seconds in the background.  
-    //  After polling the sensor, call the listener function to send the sensor data to the CoAP server or Collector Node.
-    //  If this is the Collector Node, we shall wait for sensor data from the Sensor Nodes and transmit to the CoAP server.
-    listen_sensor::start_sensor_listener()
+    //  Starting polling the temperature sensor every 10 seconds in the background.
+    app_sensor::start_sensor_listener()
         .expect("TMP fail");
+
+    //  Start the Server Transport for sending sensor data to CoAP Server over NB-IoT.
+    sensor_network::start_server_transport()
+        .expect("NET fail");
 
     //  Main event loop
     loop {                            //  Loop forever...
-        unsafe {
-            os::os_eventq_run(            //  Process events...
-                os::os_eventq_dflt_get()  //  From default event queue.
-            )
-        }
+        os::eventq_run(               //  Processing events...
+            os::eventq_dflt_get()     //  From default event queue.
+                .expect("GET fail")
+        ).expect("RUN fail");
     }
     //  Never comes here.
 }
