3f2f30a93545329bbbb1d6e06bf468006edfa307
lupyuen <luppy@appkaki.com>
2019-08-04 19:03:38 +0800

    Fixed strn macro for new Rust

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 //! Mynewt Macros for calling Mynewt APIs . Import this crate into a source code crate and check the expanded macros using:
 //! ```
 //! clear ; cargo rustc -- -Z unstable-options --pretty expanded | head -20
@@ -42,14 +60,15 @@ pub fn out(item: TokenStream) -> TokenStream {
 pub fn strn(item: TokenStream) -> TokenStream {
     let item_str = item.to_string();
     let span = proc_macro2::Span::call_site();
-    println!("item: {:#?}", item_str);
-    if item_str == "(  )" {
+    //  println!("item: {:#?}", item_str);
+    if item_str.replace(" ", "") == "()" {
+        //  Expand  `(  )` (old Rust) and `()` (new Rust) to `&Strn::new( b"\0" )`
         let expanded = quote! {
             &Strn::new( b"\0" )
         };
         return expanded.into();
-    } else if item_str.starts_with("$crate::parse!") {
-        //  If `item_str` looks like `$crate::parse!(@ json device_id)`, return as is.
+    } else if item_str.contains("parse!") {
+        //  If `item_str` looks like `$crate::parse!(@ json device_id)` (old Rust) or `::mynewt::parse!(@ json &device_id)` (new Rust), return as is.
         return item;
     } else if item_str.starts_with("\"") && item_str.ends_with("\"") {
         //  Transform literal `"\"device\""` to `&Strn::new( b"device\0" )`
@@ -67,10 +86,11 @@ pub fn strn(item: TokenStream) -> TokenStream {
     match expr {
         syn::Expr::Macro(_expr) => {
             //  Transform macro `stringify ! ( value )` to `&Strn::new( b"value\0" )`
-            let expr_split: Vec<&str> = expr_str.splitn(2, "stringify ! ( ").collect();
-            let ident = expr_split[1];
-            let ident_split: Vec<&str> = ident.splitn(2, " )").collect();
-            let ident = ident_split[0].to_string() + "\0";            
+            //  macOS gives `stringify ! ( value )` but some Window machines give `stringify ! (value)`.
+            let expr_split: Vec<&str> = expr_str.splitn(2, "stringify ! (").collect();
+            let ident = expr_split[1].trim();
+            let ident_split: Vec<&str> = ident.splitn(2, ")").collect();
+            let ident = ident_split[0].trim().to_string() + "\0";            
             //  println!("ident: {:#?}", ident);
             let bytestr = syn::LitByteStr::new(ident.as_bytes(), span);
             let expanded = quote! {
@@ -93,7 +113,7 @@ pub fn strn(item: TokenStream) -> TokenStream {
     };
     //  println!("strn: {:#?}", expr);
     //  println!("strn3: {:#?}", expr_str);
-    assert!(false, "strn");  //  Not supported
+    assert!(false, "strn!() pattern not supported: {}", item_str);  //  Not supported
     let expanded = quote! { &Strn::new( b"\0" ) };
     expanded.into()
 }
