e4d440fab547fc86759a084c952cb2e289955311
lupyuen <luppy@appkaki.com>
2019-08-08 14:41:33 +0800

    Inferring types

--- rust/macros/src/infer_type.rs
+++ rust/macros/src/infer_type.rs
@@ -23,29 +23,49 @@ use proc_macro2::Span;
 use quote::{quote, quote_spanned};
 use syn::{
     parse_macro_input,
-    ArgCaptured,
-    Expr,
-    FnArg::{
-        self,
-        Captured,
-    },
-    ForeignItem,
-    ForeignItemFn,
-    Ident,
-    ItemForeignMod,
-    ReturnType,
-    Type,
-    punctuated::Punctuated,
-    spanned::Spanned,
-    token::Comma,
+    //ArgCaptured,
+    //Expr,
+    //FnArg::{
+        //self,
+        //Captured,
+    //},
+    //ForeignItem,
+    //ForeignItemFn,
+    //Ident,
+    //ItemForeignMod,
+    //ReturnType,
+    //Type,
+    //punctuated::Punctuated,
+    //spanned::Spanned,
+    //token::Comma,
 };
 
 /// Given a Rust function definition, infer the placeholder types in the function
 pub fn infer_type_internal(_attr: TokenStream, item: TokenStream) -> TokenStream {
     //  println!("attr: {:#?}", attr); println!("item: {:#?}", item);
     //  Parse the macro input as Rust function definition.
-    let input = parse_macro_input!(item as syn::ItemFn);
-    println!("input: {:#?}", input);
+    let input: syn::ItemFn = parse_macro_input!(item as syn::ItemFn);
+    //  println!("input: {:#?}", input);
+
+    //  Process the Function Declaration.
+    let decl = input.decl;
+    //  println!("decl: {:#?}", decl);
+
+    //  For each parameter...
+    for input in decl.inputs {
+        //  println!("input: {:#?}", input);
+        match input {
+            syn::FnArg::Captured(arg_captured) => {
+                println!("arg_captured: {:#?}", arg_captured);
+                let pat = arg_captured.pat;
+                println!("pat: {:#?}", quote!{ #pat }.to_string());
+            }
+            _ => { assert!(false, "Unknown input"); }
+        }
+        break;
+    }
+
+    //  Mark each parameter for Type Inference.
 
     //  Process the Block of code inside the function.
     //  For each function call...
