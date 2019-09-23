3d453e7a18d5977a350262c858a3b611850d5ea4
lupyuen <luppy@appkaki.com>
2019-09-24 00:26:25 +0800

    Added unprotect flash for Windows

--- scripts/nrf52/unprotect-flash.sh
+++ scripts/nrf52/unprotect-flash.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# Unprotect the nRF52 module using a Raspberry Pi or Jetson Nano. Tested on OpenOCD for Jetson Nano 0.10.0+dev-00932-g85a460d5 (2019-09-23-09:18)
-# From https://stackoverflow.com/questions/52308978/problem-flashing-nrf52-chip-using-openocd/54372481#54372481
+# Unprotect the nRF52 module
 
 set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
@@ -26,14 +25,3 @@ $HOME/openocd/src/openocd \
 # openocd/bin/openocd -d2 -f interface/stlink.cfg -c "transport select hla_swd" -f target/nrf52.cfg
 
 # openocd/bin/openocd -d2 -f interface/cmsis-dap.cfg -f target/nrf52.cfg
-
-exit
-
-# Test sysfs
-sudo bash
-echo 11 >/sys/class/gpio/export
-echo 25 >/sys/class/gpio/export
-ls /sys/class/gpio
-echo 11 >/sys/class/gpio/unexport
-echo 25 >/sys/class/gpio/unexport
-exit
