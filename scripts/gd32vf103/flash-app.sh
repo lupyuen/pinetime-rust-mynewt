b5c0cbc6eb89c6f7271c40124d7533bfd56c0c2b
lupyuen <luppy@appkaki.com>
2019-10-19 10:07:51 +0800

    Added flash scripts

---
+++ scripts/gd32vf103/flash-app.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#  Flash Mynewt Application on macOS and Linux
+
+set -e  #  Exit when any command fails.
+set -x  #  Echo all commands.
+
+openocd/bin/openocd \
+    -f scripts/gd32vf103/flash-init.ocd \
+    -f interface/cmsis-dap.cfg \
+    -c "transport select jtag" \
+    -f scripts/gd32vf103/gd32vf103.ocd \
+    -f scripts/gd32vf103/flash-app.ocd
