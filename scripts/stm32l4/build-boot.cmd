83bc771f35d3451e110910127d3dd7740bac9920
lupyuen <luppy@appkaki.com>
2019-10-19 18:32:38 +0800

    Fixed boot script

--- scripts/stm32l4/build-boot.cmd
+++ scripts/stm32l4/build-boot.cmd
@@ -1,10 +1,7 @@
 ::  Build Mynewt bootloader on Windows
 
-::  Add toolchain to PATH.
-set PATH=%PATH%;%cd%\xPack\RISC-V Embedded GCC\8.2.0-3.1\bin
-
 ::  Build the bootloader.
-newt\newt.exe build gd32vf103_boot
+newt\newt.exe build stm32l4_boot
 
 ::  Show the size.
-newt\newt.exe size -v gd32vf103_boot
+newt\newt.exe size -v stm32l4_boot
