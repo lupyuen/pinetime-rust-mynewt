feb2c3cbaca7d9a8c87272482567c463de2a1ce8
lupyuen <luppy@appkaki.com>
2019-10-19 19:03:23 +0800

    Fixing bootloader build

---
+++ hw/mcu/gigadevice/gd32vf103/include/mcu/fe310.h
@@ -0,0 +1,6 @@
+//  TODO: Required by repos\apache-mynewt-core\kernel\os\include\os\arch\rv32imac\os\os_arch.h
+
+#ifndef H_GD32VF103_FE310_HAL_
+#define H_GD32VF103_FE310_HAL_
+
+#endif  //  H_GD32VF103_FE310_HAL_
