37330871b82451dbc3908121fecc31f5c00d3c22
lupyuen <luppy@appkaki.com>
2019-10-21 14:15:21 +0800

    Fixed ctx.s

---
+++ hw/mcu/gigadevice/gd32vf103/include/bits.h
@@ -0,0 +1,1 @@
+//  TODO: Needed by repos/apache-mynewt-core/kernel/os/src/arch/rv32imac/ctx.s
--- hw/mcu/gigadevice/gd32vf103/include/env/encoding.h
+++ hw/mcu/gigadevice/gd32vf103/include/env/encoding.h
@@ -11,11 +11,14 @@
 //  Define aliases for RISC-V CLINT registers
 //  For Bumblebee N200: CLINT functions are implemented in TIMER registers
 
-#define CLINT_CTRL_ADDR TIMER_CTRL_ADDR
-#define CLINT_MTIME     TIMER_MTIME
-#define CLINT_MTIMECMP  TIMER_MTIMECMP
-#define CLINT_MSIP      TIMER_MSIP
+#define CLINT_MTIME       TIMER_MTIME
+#define CLINT_MTIMECMP    TIMER_MTIMECMP
+#define CLINT_MSIP        TIMER_MSIP
+#define CLINT_REG(offset) _REG32(TIMER_CTRL_ADDR, offset)
 
-#define CLINT_REG(offset) _REG32(CLINT_CTRL_ADDR, offset)
+#ifdef __ASSEMBLER__
+//  CLINT_CTRL_ADDR is used by repos/apache-mynewt-core/kernel/os/src/arch/rv32imac/ctx.s
+#define CLINT_CTRL_ADDR   TIMER_CTRL_ADDR+TIMER_MSIP
+#endif  //  __ASSEMBLER__
 
 #endif  //  __MCU_GD32VF103_ENV_PLATFORM_H__
