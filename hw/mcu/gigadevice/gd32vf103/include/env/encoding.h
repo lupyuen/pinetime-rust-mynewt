c950176a36160c0f579ad0a34ad1eefbce2e572a
lupyuen <luppy@appkaki.com>
2019-10-21 08:44:25 +0800

    Fixing os_arch_rv32imac.c

---
+++ hw/mcu/gigadevice/gd32vf103/include/env/encoding.h
@@ -0,0 +1,6 @@
+#ifndef __MCU_GD32VF103_ENV_PLATFORM_H__
+#define __MCU_GD32VF103_ENV_PLATFORM_H__
+
+#include "../../src/ext/Firmware/RISCV/drivers/riscv_encoding.h"
+
+#endif  //  __MCU_GD32VF103_ENV_PLATFORM_H__
---
+++ hw/mcu/gigadevice/gd32vf103/include/env/freedom-e300-hifive1/platform.h
@@ -0,0 +1,1 @@
+//  TODO: Needed by repos/apache-mynewt-core/kernel/os/src/arch/rv32imac/os_arch_rv32imac.c
