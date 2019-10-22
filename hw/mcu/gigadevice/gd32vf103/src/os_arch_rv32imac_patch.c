8dbfbbf6aa35f1c2f0112e9233a7bf7171ea9f16
lupyuen <luppy@appkaki.com>
2019-10-22 13:20:37 +0800

    Fixing os_arch_os_init

--- hw/mcu/gigadevice/gd32vf103/src/os_arch_rv32imac_patch.c
+++ hw/mcu/gigadevice/gd32vf103/src/os_arch_rv32imac_patch.c
@@ -29,169 +29,6 @@
 
 extern void trap_entry();
 
-#ifdef NOTUSED
-struct context_switch_frame {
-    uint32_t  pc;
-    /* Callee saved registers */
-    uint32_t  s0;
-    uint32_t  s1;
-    uint32_t  s2;
-    uint32_t  s3;
-    uint32_t  s4;
-    uint32_t  s5;
-    uint32_t  s6;
-    uint32_t  s7;
-    uint32_t  s8;
-    uint32_t  s9;
-    uint32_t  s10;
-    uint32_t  s11;
-    /* Caller saved register */
-    uint32_t  ra;
-    uint32_t  gp;
-    uint32_t  tp;
-    uint32_t  t0;
-    uint32_t  t1;
-    uint32_t  t2;
-    uint32_t  t3;
-    uint32_t  t4;
-    uint32_t  t5;
-    uint32_t  t6;
-    uint32_t  a0;
-    uint32_t  a1;
-    uint32_t  a2;
-    uint32_t  a3;
-    uint32_t  a4;
-    uint32_t  a5;
-    uint32_t  a6;
-    uint32_t  a7;
-};
-
-uint32_t
-mtime_lo(void)
-{
-    return CLINT_REG(CLINT_MTIME);
-}
-
-uint32_t
-mtime_hi(void)
-{
-    return CLINT_REG(CLINT_MTIME + 4);
-}
-
-uint64_t
-get_timer_value(void)
-{
-    while (1) {
-        uint32_t hi = mtime_hi();
-        uint32_t lo = mtime_lo();
-        if (hi == mtime_hi())
-            return ((uint64_t)hi << 32) | lo;
-    }
-}
-
-void
-set_mtimecmp(uint64_t time)
-{
-    CLINT_REG(CLINT_MTIMECMP + 4) = -1;
-    CLINT_REG(CLINT_MTIMECMP) = (uint32_t) time;
-    CLINT_REG(CLINT_MTIMECMP + 4) = (uint32_t) (time >> 32);
-}
-
-unsigned long
-get_timer_freq()
-{
-  return 32768;
-}
-
-/* XXX: determine how to deal with running un-privileged */
-/* only priv currently supported */
-uint32_t os_flags = OS_RUN_PRIV;
-
-extern struct os_task g_idle_task;
-
-#define OS_TICK_PRIO 0
-
-static int
-os_in_isr(void)
-{
-    // TODO:
-    return 0;
-}
-
-void
-timer_handler(void)
-{
-    os_time_advance(1);
-}
-
-void
-os_arch_ctx_sw(struct os_task *t)
-{
-    if ((os_sched_get_current_task() != 0) && (t != 0)) {
-        os_sched_ctx_sw_hook(t);
-    }
-
-    /*
-     * This request software interrupt that is used for contect switching
-     */
-    CLINT_REG(CLINT_MSIP) = 1;
-}
-
-os_sr_t
-os_arch_save_sr(void)
-{
-    uint32_t isr_ctx;
-
-    isr_ctx = clear_csr(mstatus, MSTATUS_MIE) & MSTATUS_MIE;
-
-    return isr_ctx;
-}
-
-void
-os_arch_restore_sr(os_sr_t isr_ctx)
-{
-    if (isr_ctx) {
-        set_csr(mstatus, MSTATUS_MIE);
-    }
-}
-
-int
-os_arch_in_critical(void)
-{
-    return !(read_csr(mstatus) & MSTATUS_MIE);
-}
-
-/* assumes stack_top will be 8 aligned */
-
-os_stack_t *
-os_arch_task_stack_init(struct os_task *t, os_stack_t *stack_top, int size)
-{
-    struct context_switch_frame *sf;
-    uint32_t *reg;
-
-    /* Get stack frame pointer */
-    sf = (struct context_switch_frame *) ((uint8_t *) stack_top - sizeof(*sf));
-    reg = &sf->a7;
-
-    /* Zero out registers except PC which will be set */
-    while (reg != &sf->pc) {
-        *reg-- = 0;
-    }
-
-    /* Set remaining portions of stack frame */
-    sf->pc = (uint32_t) t->t_func;
-    sf->a0 = (uint32_t) t->t_arg;
-
-    return (os_stack_t *) sf;
-}
-
-void
-os_arch_init(void)
-{
-    os_init_idle_task();
-}
-#endif  //  NOTUSED
-
 #define OS_TICK_PRIO 0
 
 os_error_t
@@ -301,23 +138,184 @@ os_arch_start(void)
 }
 
 #ifdef NOTUSED
-os_error_t
-os_arch_os_start(void)
-{
-    os_error_t err;
+    struct context_switch_frame {
+        uint32_t  pc;
+        /* Callee saved registers */
+        uint32_t  s0;
+        uint32_t  s1;
+        uint32_t  s2;
+        uint32_t  s3;
+        uint32_t  s4;
+        uint32_t  s5;
+        uint32_t  s6;
+        uint32_t  s7;
+        uint32_t  s8;
+        uint32_t  s9;
+        uint32_t  s10;
+        uint32_t  s11;
+        /* Caller saved register */
+        uint32_t  ra;
+        uint32_t  gp;
+        uint32_t  tp;
+        uint32_t  t0;
+        uint32_t  t1;
+        uint32_t  t2;
+        uint32_t  t3;
+        uint32_t  t4;
+        uint32_t  t5;
+        uint32_t  t6;
+        uint32_t  a0;
+        uint32_t  a1;
+        uint32_t  a2;
+        uint32_t  a3;
+        uint32_t  a4;
+        uint32_t  a5;
+        uint32_t  a6;
+        uint32_t  a7;
+    };
 
-    err = OS_ERR_IN_ISR;
-    if (os_in_isr() == 0) {
-        err = OS_OK;
-        /* should be in kernel mode here */
-        os_arch_start();
+    uint32_t
+    mtime_lo(void)
+    {
+        return CLINT_REG(CLINT_MTIME);
     }
 
-    return err;
-}
+    uint32_t
+    mtime_hi(void)
+    {
+        return CLINT_REG(CLINT_MTIME + 4);
+    }
 
-void
-software_interrupt_handler(uintptr_t mcause)
-{
-}
+    uint64_t
+    get_timer_value(void)
+    {
+        while (1) {
+            uint32_t hi = mtime_hi();
+            uint32_t lo = mtime_lo();
+            if (hi == mtime_hi())
+                return ((uint64_t)hi << 32) | lo;
+        }
+    }
+
+    void
+    set_mtimecmp(uint64_t time)
+    {
+        CLINT_REG(CLINT_MTIMECMP + 4) = -1;
+        CLINT_REG(CLINT_MTIMECMP) = (uint32_t) time;
+        CLINT_REG(CLINT_MTIMECMP + 4) = (uint32_t) (time >> 32);
+    }
+
+    unsigned long
+    get_timer_freq()
+    {
+    return 32768;
+    }
+
+    /* XXX: determine how to deal with running un-privileged */
+    /* only priv currently supported */
+    uint32_t os_flags = OS_RUN_PRIV;
+
+    extern struct os_task g_idle_task;
+
+    #define OS_TICK_PRIO 0
+
+    static int
+    os_in_isr(void)
+    {
+        // TODO:
+        return 0;
+    }
+
+    void
+    timer_handler(void)
+    {
+        os_time_advance(1);
+    }
+
+    void
+    os_arch_ctx_sw(struct os_task *t)
+    {
+        if ((os_sched_get_current_task() != 0) && (t != 0)) {
+            os_sched_ctx_sw_hook(t);
+        }
+
+        /*
+        * This request software interrupt that is used for contect switching
+        */
+        CLINT_REG(CLINT_MSIP) = 1;
+    }
+
+    os_sr_t
+    os_arch_save_sr(void)
+    {
+        uint32_t isr_ctx;
+
+        isr_ctx = clear_csr(mstatus, MSTATUS_MIE) & MSTATUS_MIE;
+
+        return isr_ctx;
+    }
+
+    void
+    os_arch_restore_sr(os_sr_t isr_ctx)
+    {
+        if (isr_ctx) {
+            set_csr(mstatus, MSTATUS_MIE);
+        }
+    }
+
+    int
+    os_arch_in_critical(void)
+    {
+        return !(read_csr(mstatus) & MSTATUS_MIE);
+    }
+
+    /* assumes stack_top will be 8 aligned */
+
+    os_stack_t *
+    os_arch_task_stack_init(struct os_task *t, os_stack_t *stack_top, int size)
+    {
+        struct context_switch_frame *sf;
+        uint32_t *reg;
+
+        /* Get stack frame pointer */
+        sf = (struct context_switch_frame *) ((uint8_t *) stack_top - sizeof(*sf));
+        reg = &sf->a7;
+
+        /* Zero out registers except PC which will be set */
+        while (reg != &sf->pc) {
+            *reg-- = 0;
+        }
+
+        /* Set remaining portions of stack frame */
+        sf->pc = (uint32_t) t->t_func;
+        sf->a0 = (uint32_t) t->t_arg;
+
+        return (os_stack_t *) sf;
+    }
+
+    void
+    os_arch_init(void)
+    {
+        os_init_idle_task();
+    }
+    
+    os_error_t
+    os_arch_os_start(void)
+    {
+        os_error_t err;
+
+        err = OS_ERR_IN_ISR;
+        if (os_in_isr() == 0) {
+            err = OS_OK;
+            /* should be in kernel mode here */
+            os_arch_start();
+        }
+
+        return err;
+    }
+
+    void
+    software_interrupt_handler(uintptr_t mcause)
+    {
+    }
 #endif  //  NOTUSED
