5f63d1aaf1890bd01a0657ccf697f8fc437c179d
lupyuen <luppy@appkaki.com>
2019-10-27 10:30:45 +0800

    Testing debug

--- scripts/gd32vf103/debug.sh
+++ scripts/gd32vf103/debug.sh
@@ -5,8 +5,8 @@ set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
 openocd/bin/openocd \
+    -c gdb_port 50000 \
     -f scripts/gd32vf103/flash-init.ocd \
     -f interface/cmsis-dap.cfg \
-    -c "transport select jtag" \
     -f scripts/gd32vf103/gd32vf103.ocd \
     -f scripts/gd32vf103/debug.ocd
