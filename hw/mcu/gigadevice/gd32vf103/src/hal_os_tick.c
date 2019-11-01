038974f1e9c068c9f94089a540dcd80a4902c993
lupyuen <luppy@appkaki.com>
2019-11-01 23:13:23 +0800

    Tick and WFI working OK

--- hw/mcu/gigadevice/gd32vf103/src/hal_os_tick.c
+++ hw/mcu/gigadevice/gd32vf103/src/hal_os_tick.c
@@ -33,12 +33,16 @@ void set_mtimecmp(uint64_t time);
 void
 os_tick_idle(os_time_t ticks)
 {
+    //  Wait for the specified number of ticks.  By default, we will use tickless sleep.
     if (MYNEWT_VAL(OS_TICKLESS_SLEEP) && ticks > 1) {
+        //  Set mtimecmp to trigger an interrupt later.  If the value of mtime is greater 
+        //  than the value of mtimecmp, then a timer interrupt is generated.
         set_mtimecmp(last_tick_time + ticks_per_ostick * ticks);
     }
-    ////  TODO: Call WFI to reduce power usage
-    ////__asm volatile ("wfi");
+    //  Wait for an interrupt, timer or otherwise.
+    __asm volatile ("wfi");
     if (MYNEWT_VAL(OS_TICKLESS_SLEEP) && ticks > 1) {
+        //  Upon receiving an interrupt, update the current time.
         timer_interrupt_handler();
     }
 }
@@ -46,16 +50,18 @@ os_tick_idle(os_time_t ticks)
 void
 os_tick_init(uint32_t os_ticks_per_sec, int prio)
 {
+    //  Set mtimecmp to trigger an interrupt 1 tick later.
     ticks_per_ostick = RTC_FREQ / os_ticks_per_sec;
     last_tick_time = get_timer_value();
     set_mtimecmp(last_tick_time + ticks_per_ostick);
-
+    //  TODO: mie is not used in ECLIC mode.
     set_csr(mie, MIP_MTIP);
 }
 
 void
 timer_interrupt_handler(void)
 {
+    //  Upon receiving an interrupt, update the current time.  Trigger an interrupt 1 tick later.
     int delta;
     int ticks;
     uint64_t time = get_timer_value();
@@ -65,6 +71,7 @@ timer_interrupt_handler(void)
 
     last_tick_time += ticks * ticks_per_ostick;
 
+    //  Set mtimecmp to trigger an interrupt 1 tick later.
     set_mtimecmp(last_tick_time + ticks_per_ostick);
 
     os_time_advance(ticks);
@@ -74,6 +81,7 @@ timer_interrupt_handler(void)
 void
 set_mtimecmp(uint64_t time)
 {
+    //  Set mtimecmp to trigger an interrupt later at the specified time.  If the value of mtime is greater than the value of mtimecmp, then a timer interrupt is generated.
     CLINT_REG(CLINT_MTIMECMP + 4) = -1;
     CLINT_REG(CLINT_MTIMECMP) = (uint32_t) time;
     CLINT_REG(CLINT_MTIMECMP + 4) = (uint32_t) (time >> 32);
