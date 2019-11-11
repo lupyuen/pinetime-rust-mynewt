63be703cbe1b69f2d1428342374093ca18741bbc
lupyuen <luppy@appkaki.com>
2019-11-11 10:41:01 +0800

    Switched I2C port 0 to 1

--- libs/mynewt_rust/src/hal.c
+++ libs/mynewt_rust/src/hal.c
@@ -16,12 +16,16 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-#include <sysinit/sysinit.h>  //  Contains all app settings consolidated from "apps/my_sensor_app/syscfg.yml" and "targets/bluepill_my_sensor/syscfg.yml"
-#include <mynewt_rust/mynewt_rust.h>
-#include <mynewt_rust/sensor_helper.h>
-#include <mynewt_rust/json_helper.h>
-
-///  Initialise the Mynewt system.  Start the Mynewt drivers and libraries.  Equivalent to `sysinit()` macro in C.
-void rust_sysinit() {
-    sysinit();
-}
+//  Used for generating Rust bindings for HAL APIs
+#include <hal/hal_bsp.h>
+#include <hal/hal_flash.h>
+#include <hal/hal_flash_int.h>
+#include <hal/hal_gpio.h>
+#include <hal/hal_i2c.h>
+#include <hal/hal_nvreg.h>
+#include <hal/hal_os_tick.h>
+#include <hal/hal_spi.h>
+#include <hal/hal_system.h>
+#include <hal/hal_timer.h>
+#include <hal/hal_uart.h>
+#include <hal/hal_watchdog.h>
