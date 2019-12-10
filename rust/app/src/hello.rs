8b27e597ce9fedc6753c28376e487ab2ceaa25ff
lupyuen <luppy@appkaki.com>
2019-12-10 13:38:26 +0800

    Clean up

--- rust/app/src/hello.rs
+++ rust/app/src/hello.rs
@@ -12,52 +12,57 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#![no_std]  ////
+#![no_std]  //  This program will run on embedded platforms
 use druid::widget::{Align, Button, Column, Label, Padding};
 use druid::{AppLauncher, LocalizedString, Widget, WindowDesc};
 
 pub fn launch() {
     //  Build a new window
     let main_window = WindowDesc::new(ui_builder);
+    //  Application state is initially 0
     let data = 0_u32;
-    //  Launch the window
+    //  Launch the window with the initial application state
     AppLauncher::with_window(main_window)
         .use_simple_logger()
         .launch(data)
         .expect("launch failed");
 }
 
-/// Build the UI for the window. The window state consists of 1 value: `count` of type `u32`.
-fn ui_builder() -> impl Widget<u32> {  //  `u32` is the window state
+/// Build the UI for the window. The application state consists of 1 value: `count` of type `u32`.
+fn ui_builder() -> impl Widget<u32> {  //  `u32` is the application state
     //  Create a line of text based on a counter value
     let text =
         LocalizedString::new("hello-counter")
         .with_arg(
             "count", 
-            |data: &u32, _env| (*data).into()
+            //  Closure that will be run to fetch the counter value
+            | data: &u32, _env | 
+                (*data).into()  //  We return the counter value in the application state
         );
     //  Create a label widget to display the text
     let label = Label::new(text);
     //  Create a button widget to increment the counter
-    //  let button = Button::<u32>::new( ////
     let button = Button::new(
-        "increment", 
-        |_ctx, data, _env| *data += 1
+        "increment",  //  Text to be shown
+        //  Closure that will be run when button is tapped
+        | _ctx, data, _env | 
+            *data += 1  //  We increment the counter
     );
 
     //  Create a column for the UI
     let mut col = Column::new();
-    //  Add the label and button widgets to the column
-    //  col.add_child(label, 1.0); ////
+    //  Add the label widget to the column, centered with padding
     col.add_child(
         Align::centered(
             Padding::new(5.0, label)
         ),
         1.0
     );
+    //  Add the button widget to the column, with padding
     col.add_child(
         Padding::new(5.0, button), 
         1.0
     );
+    //  Return the column containing the label and button widgets
     col
 }
