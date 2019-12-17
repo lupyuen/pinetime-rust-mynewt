4b218573da9c41392c65ec51f5571f71fc64310e
lupyuen <luppy@appkaki.com>
2019-12-17 14:56:43 +0800

    Adding non-blocking SPI

--- rust/mynewt/src/spi.rs
+++ rust/mynewt/src/spi.rs
@@ -15,6 +15,14 @@ const DISPLAY_HIGH: i32 = 23;  //  LCD_BACKLIGHT_{LOW,MID,HIGH} (P0.14, 22, 23):
 const SPI_NUM: i32 = DISPLAY_SPI;
 const SPI_SS_PIN: i32 = DISPLAY_CS;
 
+/// SPI settings for ST7789 display controller
+static mut SPI_SETTINGS: hal::hal_spi_settings = hal::hal_spi_settings {
+    data_order: hal::HAL_SPI_MSB_FIRST as u8,
+    data_mode:  hal::HAL_SPI_MODE3 as u8,  //  SPI must be used in mode 3. Mode 0 (the default) won't work.
+    baudrate:   8000,  //  In kHZ. Use SPI at 8MHz (the fastest clock available on the nRF52832) because otherwise refreshing will be super slow.
+    word_size:  hal::HAL_SPI_WORD_SIZE_8BIT as u8,
+};
+
 /// Non-blocking SPI transfer callback parameter
 struct spi_cb_arg {
     transfers: i32,
@@ -38,9 +46,15 @@ extern "C" fn spi_noblock_handler(_arg: *mut core::ffi::c_void, _len: i32) {
 /// Init non-blocking SPI transfer
 pub fn spi_noblock_init() {
     unsafe { hal::hal_spi_disable(SPI_NUM) };
+
+    let rc = unsafe { hal::hal_spi_config(SPI_NUM, &mut SPI_SETTINGS) };
+    assert_eq!(rc, 0, "spi config fail");
+
     let arg = unsafe { core::mem::transmute(&mut spi_cb_obj) };
     unsafe { hal::hal_spi_set_txrx_cb(SPI_NUM, Some(spi_noblock_handler), arg) };
-    unsafe { hal::hal_spi_enable(SPI_NUM) };
+
+    let rc = unsafe { hal::hal_spi_enable(SPI_NUM) };
+    assert_eq!(rc, 0, "spi enable fail");
 
     let rc = unsafe { hal::hal_gpio_init_out(SPI_SS_PIN, 1) };
     assert_eq!(rc, 0, "gpio fail");    
