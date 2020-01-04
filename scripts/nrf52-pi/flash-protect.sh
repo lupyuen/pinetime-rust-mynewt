fc28ebf31ef5e1f60a1eaeb8794ce5eb7be923e1
lupyuen <luppy@appkaki.com>
2020-01-04 15:59:52 +0800

    Updated pi flash scripts

--- scripts/nrf52-pi/flash-protect.sh
+++ scripts/nrf52-pi/flash-protect.sh
@@ -6,5 +6,5 @@ set -x  #  Echo all commands.
 
 sudo /home/pi/openocd/src/openocd \
     -s /home/pi/openocd/tcl \
-    -f swd-pi.ocd \
-    -f flash-protect.ocd
+    -f scripts/nrf52-pi/swd-pi.ocd \
+    -f scripts/nrf52-pi/flash-protect.ocd
--- scripts/nrf52-pi/flash-unprotect.sh
+++ scripts/nrf52-pi/flash-unprotect.sh
@@ -6,5 +6,5 @@ set -x  #  Echo all commands.
 
 sudo /home/pi/openocd/src/openocd \
     -s /home/pi/openocd/tcl \
-    -f swd-pi.ocd \
-    -f flash-unprotect.ocd
+    -f scripts/nrf52-pi/swd-pi.ocd \
+    -f scripts/nrf52-pi/flash-unprotect.ocd
