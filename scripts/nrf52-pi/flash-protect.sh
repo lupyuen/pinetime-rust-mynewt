3a418d16ff2694eef2a2bdf209de6c7d6b92b536
lupyuen <luppy@appkaki.com>
2020-01-04 17:26:52 +0800

    Flash bootloader and app on pi OK

--- scripts/nrf52-pi/flash-protect.sh
+++ scripts/nrf52-pi/flash-protect.sh
@@ -5,6 +5,7 @@ set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
 sudo /home/pi/openocd/src/openocd \
+    -d2 \
     -s /home/pi/openocd/tcl \
     -f scripts/nrf52-pi/swd-pi.ocd \
     -f scripts/nrf52-pi/flash-protect.ocd
