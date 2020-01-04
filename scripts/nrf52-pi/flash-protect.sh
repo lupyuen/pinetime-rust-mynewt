486ad7e9e2a4c9afb4a44e98d6c55d36787babaa
lupyuen <luppy@appkaki.com>
2020-01-04 15:26:18 +0800

    Adding flash protection

---
+++ scripts/nrf52-pi/flash-protect.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#  Enable Flash Protection on nRF52 with Raspberry Pi
+
+set -e  #  Exit when any command fails.
+set -x  #  Echo all commands.
+
+sudo /home/pi/openocd/src/openocd \
+    -s /home/pi/openocd/tcl \
+    -f swd-pi.ocd \
+    -f flash-protect.ocd
