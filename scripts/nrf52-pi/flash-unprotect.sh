56cf9a7deb9506b25f2f795010ed22e81fb978d0
Lee Lup Yuen <luppy@appkaki.com>
2020-01-18 18:45:42 +0800

    Rollback

--- scripts/nrf52-pi/flash-unprotect.sh
+++ scripts/nrf52-pi/flash-unprotect.sh
@@ -4,7 +4,7 @@
 set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
-$HOME/openocd-spi/src/openocd \
-    -s $HOME/openocd-spi/tcl \
+sudo /home/pi/openocd/src/openocd \
+    -s /home/pi/openocd/tcl \
     -f scripts/nrf52-pi/swd-pi.ocd \
     -f scripts/nrf52-pi/flash-unprotect.ocd
