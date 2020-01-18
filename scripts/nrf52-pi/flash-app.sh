0f7618d2e6721fab23022e88b474ebbd97743ae1
Lee Lup Yuen <luppy@appkaki.com>
2020-01-18 18:46:10 +0800

    Rollback

--- scripts/nrf52-pi/flash-app.sh
+++ scripts/nrf52-pi/flash-app.sh
@@ -4,7 +4,12 @@
 set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
-$HOME/openocd-spi/src/openocd \
-    -s $HOME/openocd-spi/tcl \
+#  Sleep a while for CPU to drop
+sleep 1
+
+#  Flash at the highest priority because flashing is time-sensitive
+sudo nice --20 \
+    /home/pi/openocd/src/openocd \
+    -s /home/pi/openocd/tcl \
     -f scripts/nrf52-pi/swd-pi.ocd \
     -f scripts/nrf52/flash-app.ocd
