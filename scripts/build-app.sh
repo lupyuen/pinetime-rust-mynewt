e24819c8017b8354a65add814b34483d05ec80a3
lupyuen <luppy@appkaki.com>
2020-01-18 22:15:34 +0800

    Updated debug config

--- scripts/build-app.sh
+++ scripts/build-app.sh
@@ -5,25 +5,10 @@ set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 set +x ; echo ; echo "----- Building Rust app and Mynewt OS for $rust_build_target / $mynewt_build_app..." ; set -x
 
-#  TODO: GD32VF103
-#  mynewt_build_app=gd32vf103c-start_my_sensor
-#  rust_build_target=riscv32imac-unknown-none-elf
-#  cp .vscode/launch-gd32vf103.json .vscode/launch.json
-
-#  TODO: nRF52
 mynewt_build_app=nrf52_my_sensor
 rust_build_target=thumbv7em-none-eabihf
-cp .vscode/launch-nrf52.json .vscode/launch.json
-
-#  TODO: STM32L4
-#  mynewt_build_app=stm32l4_my_sensor
-#  rust_build_target=thumbv7em-none-eabihf
-#  cp .vscode/launch-stm32l4.json .vscode/launch.json
-
-#  TODO: STM32 Blue Pill
-#  mynewt_build_app=bluepill_my_sensor
-#  rust_build_target=thumbv7m-none-eabi
-#  cp .vscode/launch-bluepill.json .vscode/launch.json
+#  TODO: On macOS and x64 Linux, should be: cp .vscode/launch-nrf52.json .vscode/launch.json
+cp .vscode/launch-nrf52-pi.json .vscode/launch.json
 
 #  Rust build profile: debug or release
 rust_build_profile=debug
