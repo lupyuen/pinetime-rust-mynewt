f5e1433cb703939583cb8c4078ffe71211b5a879
Lee Lup Yuen <luppy@appkaki.com>
2020-01-22 10:07:34 +0800

    Upgrading to openocd-spi

--- scripts/nrf52-pi/flash-boot.sh
+++ scripts/nrf52-pi/flash-boot.sh
@@ -4,12 +4,6 @@
 set -e  #  Exit when any command fails.
 set -x  #  Echo all commands.
 
-#  Sleep a while for CPU to drop
-sleep 1
-
-#  Flash at the highest priority because flashing is time-sensitive
-sudo nice --20 \
-    /home/pi/openocd/src/openocd \
-    -s /home/pi/openocd/tcl \
+openocd/bin/openocd \
     -f scripts/nrf52-pi/swd-pi.ocd \
     -f scripts/nrf52/flash-boot.ocd
