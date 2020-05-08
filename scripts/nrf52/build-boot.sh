ce20498c1a03e4ca9531f190789d0e81f7c55bf9
lupyuen <luppy@appkaki.com>
2020-05-08 21:28:17 +0800

    Updated flash bootloader script

--- scripts/nrf52/build-boot.sh
+++ scripts/nrf52/build-boot.sh
@@ -10,6 +10,6 @@ newt build nrf52_boot
 #  Show the size.
 newt size -v nrf52_boot
 
-#  Copy the disassembler and linker map to the logs folder.
-cp bin/targets/nrf52_boot/app/apps/boot_stub/boot_stub.elf.lst logs
-cp bin/targets/nrf52_boot/app/apps/boot_stub/boot_stub.elf.map logs
+#  Copy the disassembler and linker map to the logs folder. For Stub Bootloader, select "bin/targets/nrf52_boot/app/apps/boot_stub/boot_stub.elf.*"
+cp bin/targets/nrf52_boot/app/boot/mynewt/mynewt.elf.lst logs
+cp bin/targets/nrf52_boot/app/boot/mynewt/mynewt.elf.map logs
