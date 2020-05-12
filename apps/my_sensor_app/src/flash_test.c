5cc32988dd4b31e7bc81656dda2c276640dfeed0
lupyuen <luppy@appkaki.com>
2020-05-13 04:23:03 +0800

    Adding flash test

--- apps/my_sensor_app/src/flash_test.c
+++ apps/my_sensor_app/src/flash_test.c
@@ -26,114 +26,121 @@
 #include <stdio.h>
 #include <string.h>
 
-//  flash [flash-id] -- dumps sector map
-//  flash <flash-id> read <offset> <size> -- reads bytes from flash
-//  flash <flash-id> write <offset>  <size>  -- writes incrementing data pattern 0-8 to flash
-//  flash <flash-id> erase <offset> <size> -- erases flash
-static int flash_cli_cmd(const struct shell_cmd *cmd, int argc, char **argv,
-    struct streamer *streamer) {
+enum Command {
+    MAP_COMMAND = 1,
+    READ_COMMAND,
+    WRITE_COMMAND,
+    ERASE_COMMAND,
+    SPEED_COMMAND,
+};
+
+/// Dump the sector map for the flash device: 0 for internal flash ROM, 1 for external SPI flash
+static int dump_cmd(int devid) {
     const struct hal_flash *hf;
-    uint32_t off = 0, sz = 1;
-    int sec_cnt, i, devid, soff;
+    int sec_cnt, i, soff;
     char *eptr;
     char tmp_buf[32];
     char pr_str[80];
-    devid = 0;
-
-    //  devid = strtoul(argv[1], &eptr, 0);
-    if (argc < 3) {
-        do {
-            hf = hal_bsp_flash_dev(devid);
-            if (!hf) {
-                if (argc == 2) {
-                    console_printf(streamer, "Flash device not present\n");
-                }
-                return 0;
-            }
-            console_printf(streamer, "Flash %d at 0x%lx size 0x%lx with %d "
-                                      "sectors, alignment req %d bytes\n",
-                    devid,
-                    (long unsigned int) hf->hf_base_addr,
-                    (long unsigned int) hf->hf_size,
-                    hf->hf_sector_cnt,
-                    hf->hf_align);
-            sec_cnt = hf->hf_sector_cnt;
-            if (sec_cnt > 32) {
-                sec_cnt = 32;
-            }
-            for (i = 0; i < sec_cnt; i++) {
-                console_printf(streamer, "  %d: %lx\n", i,
-                        (long unsigned int) hal_flash_sector_size(hf, i));
-            }
-            if (sec_cnt != hf->hf_sector_cnt) {
-                console_printf(streamer, "...  %d: %lx\n",
-                  hf->hf_sector_cnt - 1,
-                  (long unsigned int) hal_flash_sector_size(hf, hf->hf_sector_cnt - 1));
-            }
-            ++devid;
-        } while(argc == 1);
-        return 0;
+    hf = hal_bsp_flash_dev(devid);
+    if (!hf) {
+        console_printf("Flash device not present\n");
+        return -1;
     }
-
-    //  off = strtoul(argv[3], &eptr, 0);
-    //  sz = strtoul(argv[4], &eptr, 0);
-    if (!strcmp(argv[2], "erase")) {
-        console_printf(streamer, "Erase 0x%lx + %lx\n",
-                (long unsigned int) off, (long unsigned int) sz);
-
-        if (hal_flash_erase(devid, off, sz)) {
-            console_printf(streamer, "Flash erase failed\n");
-        }
-        console_printf(streamer, "Done!\n");
-    } else if (!strcmp(argv[2], "read")) {
-        console_printf(streamer, "Read 0x%lx + %lx\n",
-                (long unsigned int) off, (long unsigned int) sz);
-        sz += off;
-        while (off < sz) {
-            sec_cnt = min(sizeof(tmp_buf), sz - off);
-            if (hal_flash_read(devid, off, tmp_buf, sec_cnt)) {
-                console_printf(streamer, "flash read failure at %lx\n",
-                        (long unsigned int) off);
-                break;
-            }
-            for (i = 0, soff = 0; i < sec_cnt; i++) {
-                soff += snprintf(pr_str + soff, sizeof(pr_str) - soff,
-                  "0x%02x ", tmp_buf[i] & 0xff);
-                if (i % 8 == 7) {
-                    console_printf(streamer, "  0x%lx: %s\n",
-                                   (long unsigned int) off, pr_str);
-                    soff = 0;
-                    off += 8;
-                }
-            }
-            if (i % 8) {
-                console_printf(streamer, "  0x%lx: %s\n",
-                               (long unsigned int) off, pr_str);
-                off += i;
-            }
-        }
-    } else if (!strcmp(argv[2], "write")) {
-        console_printf(streamer, "Write 0x%lx + %lx\n",
-                (long unsigned int) off, (long unsigned int) sz);
-
-        sz += off;
-        for (i = 0; i < sizeof(tmp_buf); i++) {
-            tmp_buf[i] = i + 1;
-        }
-
-        while (off < sz) {
-            sec_cnt = min(sizeof(tmp_buf), sz - off);
-            if (hal_flash_write(devid, off, tmp_buf, sec_cnt)) {
-                console_printf(streamer, "flash write failure at %lx\n",
-                        (long unsigned int) off);
-            }
-            off += sec_cnt;
-        }
-        console_printf(streamer, "Done!\n");
+    console_printf("Flash %d at 0x%lx size 0x%lx with %d "
+                                "sectors, alignment req %d bytes\n",
+        devid,
+        (long unsigned int) hf->hf_base_addr,
+        (long unsigned int) hf->hf_size,
+        hf->hf_sector_cnt,
+        hf->hf_align);
+    sec_cnt = hf->hf_sector_cnt;
+    if (sec_cnt > 32) {
+        sec_cnt = 32;
+    }
+    for (i = 0; i < sec_cnt; i++) {
+        console_printf("  %d: %lx\n", i,
+                (long unsigned int) hal_flash_sector_size(hf, i));
+    }
+    if (sec_cnt != hf->hf_sector_cnt) {
+        console_printf("...  %d: %lx\n",
+            hf->hf_sector_cnt - 1,
+            (long unsigned int) hal_flash_sector_size(hf, hf->hf_sector_cnt - 1));
     }
     return 0;
-err:
-    return -1;
+}
+
+//  flash [flash-id] -- dumps sector map
+//  flash <flash-id> read <offset> <size> -- reads bytes from flash
+//  flash <flash-id> write <offset> <size> -- writes incrementing data pattern 0-8 to flash
+//  flash <flash-id> erase <offset> <size> -- erases flash
+static int flash_cmd(enum Command cmd, int devid, uint32_t off, uint32_t sz) {
+    //  Previously defaults to off = 0, sz = 1
+    const struct hal_flash *hf;
+    int sec_cnt, i, soff;
+    char *eptr;
+    char tmp_buf[32];
+    char pr_str[80];
+    switch(cmd) {
+        ERASE_COMMAND: {
+            console_printf("Erase 0x%lx + %lx\n",
+                (long unsigned int) off, (long unsigned int) sz);
+            if (hal_flash_erase(devid, off, sz)) {
+                console_printf("Flash erase failed\n");
+            }
+            console_printf("Done!\n");
+            break;
+        }
+        READ_COMMAND: {
+            console_printf("Read 0x%lx + %lx\n",
+                (long unsigned int) off, (long unsigned int) sz);
+            sz += off;
+            while (off < sz) {
+                sec_cnt = min(sizeof(tmp_buf), sz - off);
+                if (hal_flash_read(devid, off, tmp_buf, sec_cnt)) {
+                    console_printf("flash read failure at %lx\n",
+                        (long unsigned int) off);
+                    break;
+                }
+                for (i = 0, soff = 0; i < sec_cnt; i++) {
+                    soff += snprintf(pr_str + soff, sizeof(pr_str) - soff,
+                    "0x%02x ", tmp_buf[i] & 0xff);
+                    if (i % 8 == 7) {
+                        console_printf("  0x%lx: %s\n",
+                            (long unsigned int) off, pr_str);
+                        soff = 0;
+                        off += 8;
+                    }
+                }
+                if (i % 8) {
+                    console_printf("  0x%lx: %s\n",
+                        (long unsigned int) off, pr_str);
+                    off += i;
+                }
+            }
+            break;
+        }
+        WRITE_COMMAND: {
+            console_printf("Write 0x%lx + %lx\n",
+                (long unsigned int) off, (long unsigned int) sz);
+            sz += off;
+            for (i = 0; i < sizeof(tmp_buf); i++) {
+                tmp_buf[i] = i + 1;
+            }
+            while (off < sz) {
+                sec_cnt = min(sizeof(tmp_buf), sz - off);
+                if (hal_flash_write(devid, off, tmp_buf, sec_cnt)) {
+                    console_printf("flash write failure at %lx\n",
+                        (long unsigned int) off);
+                }
+                off += sec_cnt;
+            }
+            console_printf("Done!\n");
+            break;
+        }
+        default:
+            break;
+    }
+    return 0;
 }
 
 //  Returns # of ops done within 2 seconds.
@@ -177,8 +184,7 @@ int flash_speed_test(int flash_dev, uint32_t addr, int sz, int move) {
 }
 
 //  flash_speed <flash_id> <addr> <rd_sz>|range [move]
-static int flash_speed_test_cli(const struct shell_cmd *cmd, int argc, char **argv,
-    struct streamer *streamer) {
+static int flash_speed_test_cli() {
     char *ep;
     int flash_dev;
     uint32_t addr, sz;
@@ -195,23 +201,23 @@ static int flash_speed_test_cli(const struct shell_cmd *cmd, int argc, char **ar
     if (i == 0) {
         //  For size
         //  sz = strtoul(argv[3], &ep, 0);
-        console_printf(streamer,
+        console_printf(
           "Speed test, hal_flash_read(%d, 0x%x%s, %d)\n",
           flash_dev, (unsigned int)addr, move?"..":"", (unsigned int)sz);
         cnt = flash_speed_test(flash_dev, addr, sz, move);
-        console_printf(streamer, "%d\n", cnt >> 1);
+        console_printf("%d\n", cnt >> 1);
     } else {
         //  For range
         uint32_t sizes[] = {
             1, 2, 4, 8, 16, 24, 32, 48, 64, 96, 128, 192, 256
         };
-        console_printf(streamer,
+        console_printf(
           "Speed test, hal_flash_read(%d, 0x%x%s, X)\n",
           flash_dev, (unsigned int)addr, move?"..":"");
 
         for (i = 0; i < sizeof(sizes) / sizeof(sizes[0]); i++) {
             cnt = flash_speed_test(flash_dev, addr, sizes[i], move);
-            console_printf(streamer, "%3d %d\n", (int)sizes[i], cnt >> 1);
+            console_printf("%3d %d\n", (int)sizes[i], cnt >> 1);
             os_time_delay(OS_TICKS_PER_SEC / 8);
         }
     }
