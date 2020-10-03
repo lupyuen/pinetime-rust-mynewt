a5da20bfb26dd95772650322988c447dcaf51a1b
lupyuen <luppy@appkaki.com>
2020-10-03 18:48:06 +0800

    Disable time sync

--- apps/my_sensor_app/src/ble_main.c
+++ apps/my_sensor_app/src/ble_main.c
@@ -322,16 +322,18 @@ bleprph_gap_event(struct ble_gap_event *event, void *arg)
             phy_conn_changed(event->connect.conn_handle);
 #endif
 
-            //  When a BLE connection is established, we read the GATT Characteristic of the Current Time Service of the BLE Peer
-            //  Based on https://github.com/apache/mynewt-nimble/blob/master/apps/blecent/src/main.c
+            if (false) {
+                //  When a BLE connection is established, we read the GATT Characteristic of the Current Time Service of the BLE Peer
+                //  Based on https://github.com/apache/mynewt-nimble/blob/master/apps/blecent/src/main.c
 
-            //  Remember the BLE Peer
-            rc = blepeer_add(event->connect.conn_handle);
-            if (rc != 0) { MODLOG_DFLT_ERROR("Failed to add peer: %d\n", rc); MODLOG_DFLT_FLUSH(); }
-            else {
-                //  Discover all GATT Sevices in BLE Peer (including Current Time Service)
-                rc = blepeer_disc_all(event->connect.conn_handle, blecent_on_disc_complete, NULL);
-                if (rc != 0) { MODLOG_DFLT_ERROR("Failed to discover services: %d\n", rc); MODLOG_DFLT_FLUSH(); }
+                //  Remember the BLE Peer. Ignore the error if we have already added the peer.
+                rc = blepeer_add(event->connect.conn_handle);
+                if (rc != 0 && rc != 2) { MODLOG_DFLT_ERROR("Failed to add peer: %d\n", rc); MODLOG_DFLT_FLUSH(); }
+                else {
+                    //  Discover all GATT Sevices in BLE Peer (including Current Time Service)
+                    rc = blepeer_disc_all(event->connect.conn_handle, blecent_on_disc_complete, NULL);
+                    if (rc != 0) { MODLOG_DFLT_ERROR("Failed to discover services: %d\n", rc); MODLOG_DFLT_FLUSH(); }
+                }
             }
         }
         MODLOG_DFLT_INFO("\n");
